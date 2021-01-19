cd86172973d6f404774ac719351d6dd5404d4b2a
Kieran Venison <venison@hey.com>
2021-01-19 08:01:49 +0000

    finished starter project

---
+++ starter-files/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Styled Components. {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
---
+++ starter-files/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Services</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
