51c7509a22b3122e35c237656f3cb2f3e215884b
Kieran Venison <venison@hey.com>
2021-01-19 08:06:19 +0000

    matched the solution files to starter files

--- solution/src/index.js
+++ solution/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HomePage from "./pages";
 
 function App() {
   return (
     <>
+      <Header />
       <HomePage />
+      <Footer />
     </>
   );
 }
--- solution/src/pages/index.js
+++ solution/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>Styled Components</h1>
     </div>
   );
 };
