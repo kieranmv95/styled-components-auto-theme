4b4387380059311ab60d650208293c07e7e97138
Kieran Venison <venison@hey.com>
2021-01-22 18:57:18 +0000

    copied to starter files

--- starter-files/src/components/Footer.js
+++ starter-files/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      <p>Styled Components. {new Date().getFullYear()}</p>
+      <p>Styled Components. {new Date().getFullYear()}. Made with ❤️</p>
     </footer>
   );
 }
--- starter-files/src/pages/index.js
+++ starter-files/src/pages/index.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function HomePage() {
   return (
     <div>
+      <img
+        src="https://www.styled-components.com/atom.png"
+        alt="Styled Components Logo"
+      />
       <h1>Styled Components</h1>
+      <p>Use the best bits of ES6 and CSS to style your apps without stress</p>
+      <button>GitHub</button>
+      <button>Documentation</button>
     </div>
   );
 };
